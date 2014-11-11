0e07c65aa790296cc7b0f230f0d749a8a5bad267
System Administrator <root@kwanchileungtekiMacBook-Pro.local>
2014-11-12 00:56:49 +0800

    commit

--- class.js
+++ class.js
@@ -1,10 +1,13 @@
-/* 展示组件的基类
+/* 展示组件类
 	public properties:
  		obj shape : Shape实例
  		obj pos   : 坐标信息，x & y
  		obj stage : 公共的Stage实例
+
  	public methods:
-		update move(x,y)
+		update()	: 更新视图
+		move(x,y)  	: 将组件移动{x,y}个单位
+		moveTo(x,y)	: 将组件移动至{x,y}位置
 */
 var DisplayObj = Object.subClass({
 	init: function() {
@@ -39,14 +42,30 @@ var DisplayObj = Object.subClass({
 		this.pos.y += y;
 		this.update();
 	},
+	moveTo: function(x, y) {
+		this.pos.x = x;
+		this.pos.y = y;
+		this.update();
+	},
 	stage: config.stage
 });
 
-// 容器的基类
-// expose properties:
-// container stage pos(x,y)
-// expose methods:
-// addChild removeChild update move(x,y) moveTo(x,y)
+/* 容器组件类
+	public properties:
+		obj container : Container实例
+		obj stage 	  : 公共的Stage实例
+		obj pos 	  : 坐标信息，x & y
+
+	private properties:
+		obj _offset    : 触摸点距离坐标信息的距离，x & y
+		
+	public methods:
+		addChild(child)		: 添加子组件到Container实例中，接受多种参数格式：单个对象、对象数组、未命名参数对象 
+		removeChild(child)	: 从Container实例中删除子组件，成功则返回true
+		update				: 更新视图
+		move(x,y)  			: 将组件移动{x,y}个单位
+		moveTo(x,y)			: 将组件移动至{x,y}位置
+*/
 var Container = Object.subClass({
 	init: function() {
 		var that = this;
@@ -54,6 +73,38 @@ var Container = Object.subClass({
 		this.container = new createjs.Container();
 
 		this.stage.addChild(this.container);
+
+		var _offset = {
+			x: 0,
+			y: 0
+		};
+
+		this.config = Object.defineProperties({}, {
+			enableMoving: {
+				get: function(value) {
+					return this.value || false;
+				},
+				set: function(value) {
+					this.value = value;
+
+					if (this.value) {
+						// 绑定事件
+						that.container.on('mousedown', function(evt) {
+							_offset.x = that.pos.x - evt.stageX;
+							_offset.y = that.pos.y - evt.stageY;
+						});
+
+						that.container.on('pressmove', function(evt) {
+							that.moveTo(evt.stageX + _offset.x, evt.stageY + _offset.y);
+						});
+					} else {
+						// 解绑事件
+						that.container.off('mousedown');
+						that.container.off('pressmove');
+					}
+				}
+			}
+		});
 
 		this.pos = Object.defineProperties({}, {
 			x: {
@@ -74,21 +125,6 @@ var Container = Object.subClass({
 					that.update();
 				}
 			}
-		});
-
-		var offset = {
-			x: 0,
-			y: 0
-		};
-
-		// 事件绑定
-		this.container.on('mousedown', function(evt) {
-			offset.x = that.pos.x - evt.stageX;
-			offset.y = that.pos.y - evt.stageY;
-		});
-
-		this.container.on('pressmove', function(evt) {
-			that.moveTo(evt.stageX + offset.x, evt.stageY + offset.y);
 		});
 	},
 	addChild: function(child) {
@@ -130,4 +166,48 @@ var Container = Object.subClass({
 		this.update();
 	},
 	stage: config.stage
+});
+
+/* 积木类 extend Container
+	public properties:
+		littleSquareList : 小正方集合
+		shapeDesc		 : 形状描述
+
+	public methods:
+		releaseLittleSquare(square)	: 将小正方从自身中释放，成功则返回true
+		setOut()					: 堆叠成积木
+*/
+var Brick = Container.subClass({
+	init: function() {
+		this._super();
+
+		// 引用类型的值不可以作为父类共享属性
+		this.shapeDesc = [];
+
+		var that = this;
+		this.container.on('pressup', function(evt) {
+			that.kursaal.settle(that);
+		});
+	},
+	releaseLittleSquare: function(square) {
+		// 仅仅是释放子组件，并没有更新shapeDesc里的关系
+		return this.removeChild(square.shape);
+	},
+	setOut: function() {
+		var that = this;
+
+		this.shapeDesc.forEach(function(val, key, arr) {
+			if (!val.isSet) {
+				var square = new LittleSquare();
+				that.addChild(square.shape);
+
+				square.moveTo(val.x * config.size, val.y * config.size);
+				arr[key].item = square;
+				arr[key].isSet = true;
+			}
+		});
+	},
+	setKursaal: function(kursaal) {
+		this.kursaal = kursaal;
+	}
 });
--- script.js
+++ script.js
@@ -1,76 +1,3 @@
-// 积木基类
-// expose properties:
-// littleSquareList shapeDesc
-// expose methods:
-// addLittleSquare removeLittleSquare setOut
-var Brick = Container.subClass({
-	init: function() {
-		this._super();
-
-		// 引用类型的值不可以作为父类共享属性
-		this.shapeDesc = [];
-		this.littleSquareList = [];
-	},
-
-	addLittleSquare: function(square) {
-		// 检测是否还需要添加小正方
-		if (typeof this.shapeDesc.curIndex === 'number') {
-			// 如果有索引记录的话，检测这个指向shapeDesc数组元素的索引
-			if (this.shapeDesc.curIndex >= this.shapeDesc.length) {
-				// 已经满员了~
-				return false;
-			}
-		} else {
-			// 如无索引记录，建立一个索引
-			this.shapeDesc.curIndex = 0;
-		}
-
-		if (Object.prototype.toString.apply(square) === '[object Array]' && square.length > 0) {
-			if (typeof square[0] === 'object' && square[0] instanceof LittleSquare) {
-				var that = this;
-				square.forEach(function(val) {
-					that.shapeDesc[that.shapeDesc.curIndex++].item = val;
-					if (that.shapeDesc.curIndex >= that.shapeDesc.length) {
-						return false;
-					}
-				});
-			}
-		} else if (typeof square === 'object' && square instanceof LittleSquare) {
-			this.shapeDesc[this.shapeDesc.curIndex++].item = square;
-		}
-	},
-	removeLittleSquare: function(square) {
-		this.removeChild(square.shape);
-
-		return square;
-	},
-	setOut: function() {
-		var that = this,
-			flag = true;
-
-		this.shapeDesc.forEach(function(val, key, arr) {
-			if (!!val.item) {
-				if (!val.isSet) {
-					that.addChild(val.item.shape);
-					val.item.pos.x = val.x * config.size;
-					val.item.pos.y = val.y * config.size;
-					arr[key].isSet = true;
-				}
-			} else {
-				flag = false;
-				return flag;
-			}
-		});
-
-		if (!flag) {
-			return {
-				succ: false,
-				msg: 'little squares are not enough'
-			};
-		}
-	}
-});
-
 // 具体某种积木类
 var I = Brick.subClass({
 	init: function(shapeDesc) {
@@ -78,12 +5,42 @@ var I = Brick.subClass({
 		this.shapeDesc = shapeDesc || [];
 	}
 });
+// 小正方型
+var LittleSquare = DisplayObj.subClass({
+	init: function(size, color) {
+		this._super();
+		this.color = color || config.color;
+		this.width = size || config.size;
+		this.height = size || config.size;
 
-// 游乐场
-// expose properties:
-// map[][] mapWidth mapHeight
-// expose methods:
-// contain(brick) 
+		// initialize shape
+		// todo
+		this.shape.graphics.beginFill('red').drawRect(0, 0, 50, 50);
+	}
+});
+// 随机积木生成器
+var RandomBrickGenerator = Container.subClass({
+	init: function() {
+		this._super();
+	},
+	random: function() {
+		// todo
+		// 生成一个随机形状的积木
+	}
+});
+
+/* 游乐场类 extend Container
+	public properties:
+		arr map 	: 一个二维的正方地图
+		mapWidth	: 地图宽度
+		mapHeight 	: 地图高度
+	
+	public methods:
+		addLittleSquare		: 添加小正方
+		removeLittleSquare	: 删除小正方
+		contain(brick) 		: 积木brick是否在游乐场范围内，返回bool
+		settle(brick)  		: 将积木安装在游乐场中，失败返回false
+*/
 var Kursaal = Container.subClass({
 	init: function() {
 		this._super();
@@ -91,12 +48,12 @@ var Kursaal = Container.subClass({
 			var ret = new Array(config.mapSize);
 			for (var i = 0, len = ret.length; i < len; i++) {
 				var arr = new Array(config.mapSize);
-				arr.forEach(function(v, k, m) {
-					m[k] = {
+				for (var j = 0, jlen = arr.length; j < jlen; j++) {
+					arr[j] = {
 						square: null,
 						isEmpty: true
 					};
-				});
+				}
 				ret[i] = arr;
 			}
 			return ret;
@@ -104,6 +61,17 @@ var Kursaal = Container.subClass({
 
 		this.mapWidth = config.mapSize * config.size;
 		this.mapHeight = config.mapSize * config.size;
+
+		// temp todo
+		var test = new createjs.Shape();
+		test.graphics.beginFill('yellow').drawRect(0, 0, this.mapWidth, this.mapHeight);
+		this.addChild(test);
+	},
+	addLittleSquare: function(square) {
+		this.addChild(square.shape);
+	},
+	removeLittleSquare: function(square) {
+		return this.removeChild(square.shape);
 	},
 	settle: function(brick) {
 		if (!(brick instanceof Brick)) {
@@ -124,10 +92,11 @@ var Kursaal = Container.subClass({
 			available = true,
 			coordinates = [];
 
-		brick.littleSquareList.forEach(function(val, key, arr) {
+		brick.shapeDesc.forEach(function(val, key, arr) {
 			// 计算出积木中每个正方所落在的位置
-			var squareX = val.pos.x + brick.pos.x,
-				squareY = val.pos.y + brick.pos.y,
+			var square = val.item,
+				squareX = square.pos.x + brick.pos.x,
+				squareY = square.pos.y + brick.pos.y,
 				locaX = 0,
 				locaY = 0;
 
@@ -160,19 +129,19 @@ var Kursaal = Container.subClass({
 		}
 
 		// 若可行，则放置积木
-		coordinates.forEach(function(coordinate){
-			var square = brick.littleSquareList[coordinate.listIndex];
+		coordinates.forEach(function(coordinate) {
+			var square = brick.shapeDesc[coordinate.listIndex].item;
 
 			// 1.更新所属关系
-			that.addLittleSquare(brick.removeLittleSquare(square));
+			brick.releaseLittleSquare(square);
+			that.addLittleSquare(square);
 
 			// 2.更新map状态
 			that.map[coordinate.x][coordinate.y].square = square;
 			that.map[coordinate.x][coordinate.y].isEmpty = false;
 
-			// 3.更新正方位置
-			square coordinate.x
-			coordinate.y
+			// 3.更新小正方位置
+			square.moveTo(coordinate.x * config.size, coordinate.y * config.size);
 		});
 	},
 	contain: function(brick) {
@@ -184,16 +153,17 @@ var Kursaal = Container.subClass({
 			flag = true;
 
 		// 检测积木里所有的正方是否都落在了游乐场里
-		brick.littleSquareList.forEach(function(val, key, arr) {
+		brick.shapeDesc.forEach(function(val, key, arr) {
 			// 位置的比较需要一致的参照物，在这里应该是stage
-			var squareX = val.pos.x + brick.x,
-				squareY = val.pos.y + brick.y;
+			var square = val.item,
+				squareX = square.pos.x + brick.pos.x,
+				squareY = square.pos.y + brick.pos.y;
 
-			if (squareX >= that.x && squareX <= (that.x + that.mapWidth - config.size) && squareY >= that.y && squareY <= (that.y + that.mapHeight - config.size)) {
+			if (squareX >= that.pos.x && squareX <= (that.pos.x + that.mapWidth - config.size) && squareY >= that.pos.y && squareY <= (that.pos.y + that.mapHeight - config.size)) {
 				return true;
 			} else {
 				flag = false;
-				return flag;
+				return false;
 			}
 		});
 
@@ -209,34 +179,8 @@ var Kursaal = Container.subClass({
 	}
 });
 
-// 小正方型
-var LittleSquare = DisplayObj.subClass({
-	init: function(size, color) {
-		this._super();
-		this.color = color || config.color;
-		this.width = size || config.size;
-		this.height = size || config.size;
-
-		// initialize shape
-		// todo
-		this.shape.graphics.beginFill('red').drawRect(0, 0, 50, 50);
-	}
-});
-
-// 随机积木生成器
-var RandomBrickGenerator = Container.subClass({
-	init: function() {
-		this._super();
-	},
-	random: function() {
-		// todo
-		// 生成一个随机形状的积木
-	}
-});
-
-var playground = new Kursaal();
-
 // main
+var playground = new Kursaal();
 var demo = new I([{
 	x: 2,
 	y: 2
@@ -247,10 +191,6 @@ var demo = new I([{
 	x: 2,
 	y: 4
 }]);
-
-demo.addLittleSquare([new LittleSquare(), new LittleSquare(), new LittleSquare()]);
 demo.setOut();
-demo.update();
-
-// playground.settle(demo);
-// playground.update();
+demo.setKursaal(playground);
+playground.update();
