ffc613e949fb41da03d6f046811cf8062bd65ede
guanzl <guanzl@ucweb.com>
2014-11-12 21:06:01 +0800

    [UPD]抽出一些类代码到class.js

--- script.js
+++ script.js
@@ -1,11 +1,76 @@
-// 随机积木生成器
+/* 随机积木生成器 extend Container
+	public properties:
+		arr brickList : 现存积木列表
+		obj kursaal   : 要服务的娱乐场的引用
+
+	private properties:
+		arr _randomList : 可供生成器选择的积木类型列表
+
+	public methods:
+		random  	: 生成若干个随机形状的积木,放回到自己的brickList
+		display 	: 展示现存的积木
+		setKursaal	: 设置要服务的娱乐场
+
+	private methods:
+		_pressUpHandler : 鼠标松开事件handler
+*/
 var RandomBrickGenerator = Container.subClass({
 	init: function() {
 		this._super();
+
+		this.brickList = [];
+		this.kursaal = null;
+
+		this.container.on('pressup', this._pressUpHandler, this);
+	},
+	setKursaal: function(kursaal) {
+		this.kursaal = kursaal;
 	},
 	random: function() {
+		if (this.brickList.length !== 0) {
+			return this;
+		}
+		for (var i = 0; i < config.randomCount; i++) {
+			this.brickList.push(new Brick(this._randomList[parseInt(Math.random() * this._randomList.length)]));
+		}
+		return this;
+	},
+	display: function() {
+		if (!!!this.kursaal) {
+			return false;
+		}
+
+		var that = this;
+
+		// 设置积木的属性
+		this.brickList.forEach(function(brick) {
+			that.addChild(brick.container);
+			brick.setKursaal(that.kursaal);
+			brick.config.enableMoving = true;
+		});
+
+		// this.update();
 		// todo
-		// 生成一个随机形状的积木
+	},
+	start: function() {
+		this.random().display();
+	},
+	_randomList: (function() {
+		var ret = [];
+		for (var shapeName in config.shapes) {
+			ret.push(shapeName);
+		}
+		return ret;
+	})(),
+	_pressUpHandler: function() {
+		this._super();
+
+		// todo
+
+		// extend
+		if (this.brickList.length === 0) {
+			this.random().display();
+		}
 	}
 });
 
@@ -13,19 +78,10 @@ var RandomBrickGenerator = Container.subClass({
 
 // main
 var playground = new Kursaal();
+var generator = new RandomBrickGenerator();
 
-var demo1 = new VerticalLine2();
-var demo2 = new VerticalLine2();
+// 设置生成器要服务的娱乐场
+generator.setKursaal(playground);
 
-demo2.move(100, 0);
-
-demo1.setOut();
-demo2.setOut();
-
-demo1.setKursaal(playground);
-demo2.setKursaal(playground);
-
-demo1.config.enableMoving = true;
-demo2.config.enableMoving = true;
-
-// playground.update();
+// 启动游戏
+generator.start();
