032174342918b1edbabdb94bd34ce5a6ec6afe09
guanzl <guanzl@ucweb.com>
2014-11-12 17:55:49 +0800

    [UPD]形状描述用json格式

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 // 基础配置
 var config = {
-	mapSize: 10,
+	mapSize: 4,
 	size: 50,
 	color: 'red',
 	canvasElm: 'myCanvas',
-	stage: new createjs.Stage('myCanvas')
+	stage: new createjs.Stage('myCanvas'),
+	shapes: {
+		verticalLine2: '[{"x": 2, "y": 2}, {"x": 2,"y": 3}]'
+	}
 };
