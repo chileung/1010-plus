05e26e14e6fb58cc8a148b976a0882ab4b41099e
guanzl <guanzl@ucweb.com>
2014-11-12 21:06:16 +0800

    [UPD]添加各种具体的积木的形状描述配置

--- config.js
+++ config.js
@@ -2,10 +2,24 @@
 var config = {
 	mapSize: 4,
 	size: 50,
+	randomCount: 3,
 	color: 'red',
 	canvasElm: 'myCanvas',
 	stage: new createjs.Stage('myCanvas'),
 	shapes: {
-		verticalLine2: '[{"x": 2, "y": 2}, {"x": 2,"y": 3}]'
+		point1: '[{"x": 0, "y": 0}]',
+		verticalLine2: '[{"x": 0, "y": 0}, {"x": 0,"y": 1}]',
+		horizontalLine2: '[{"x": 0, "y": 0}, {"x": 1,"y": 0}]',
+		leftTopL3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 0}]',
+		leftBottomL3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 1}]',
+		rightTopL3: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 1, "y": 1}]',
+		rightBottomL3: '[{"x": 1, "y": 0}, {"x": 1, "y": 1}, {"x": 0, "y": 1}]',
+		verticalLine3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}]',
+		horizontalLine3: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}]',
+		square4: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 0}, {"x": 1, "y": 1}]',
+		verticalLine4: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}, {"x": 0, "y": 3}]',
+		horizontalLine4: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}, {"x": 3, "y": 0}]',
+		verticalLine5: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}, {"x": 0, "y": 3}, {"x": 0, "y": 4}]',
+		horizontalLine5: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}, {"x": 3, "y": 0}, {"x": 4, "y": 0}]'
 	}
 };
