6dda46971224efb80757129a947dac969718a12c
guanzl <guanzl@ucweb.com>
2014-11-13 15:14:20 +0800

    [UPD]更新配置

--- config.js
+++ config.js
@@ -1,25 +1,29 @@
 // 基础配置
-var config = {
-	mapSize: 10,
-	size: 50,
-	randomCount: 1,
-	color: 'red',
-	canvasElm: 'myCanvas',
-	stage: new createjs.Stage('myCanvas'),
-	shapes: {
-		point1: '[{"x": 0, "y": 0}]',
-		verticalLine2: '[{"x": 0, "y": 0}, {"x": 0,"y": 1}]',
-		horizontalLine2: '[{"x": 0, "y": 0}, {"x": 1,"y": 0}]',
-		leftTopL3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 0}]',
-		leftBottomL3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 1}]',
-		rightTopL3: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 1, "y": 1}]',
-		rightBottomL3: '[{"x": 1, "y": 0}, {"x": 1, "y": 1}, {"x": 0, "y": 1}]',
-		verticalLine3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}]',
-		horizontalLine3: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}]',
-		square4: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 0}, {"x": 1, "y": 1}]',
-		verticalLine4: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}, {"x": 0, "y": 3}]',
-		horizontalLine4: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}, {"x": 3, "y": 0}]',
-		verticalLine5: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}, {"x": 0, "y": 3}, {"x": 0, "y": 4}]',
-		horizontalLine5: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}, {"x": 3, "y": 0}, {"x": 4, "y": 0}]'
-	}
-};
+(function(that) {
+	var myStage = new createjs.Stage('myCanvas');
+	that.config = {
+		mapSize: 10,
+		size: parseInt(myStage.canvas.width / 12),
+		randomCount: 3,
+		canvasElm: 'myCanvas',
+		stage: myStage,
+		shapes: {
+			point1: '[{"x": 0, "y": 0}]',
+			verticalLine2: '[{"x": 0, "y": 0}, {"x": 0,"y": 1}]',
+			horizontalLine2: '[{"x": 0, "y": 0}, {"x": 1,"y": 0}]',
+			leftTopL3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 0}]',
+			leftBottomL3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 1}]',
+			rightTopL3: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 1, "y": 1}]',
+			rightBottomL3: '[{"x": 1, "y": 0}, {"x": 1, "y": 1}, {"x": 0, "y": 1}]',
+			verticalLine3: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}]',
+			horizontalLine3: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}]',
+			square4: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 1, "y": 0}, {"x": 1, "y": 1}]',
+			verticalLine4: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}, {"x": 0, "y": 3}]',
+			horizontalLine4: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}, {"x": 3, "y": 0}]',
+			verticalLine5: '[{"x": 0, "y": 0}, {"x": 0, "y": 1}, {"x": 0, "y": 2}, {"x": 0, "y": 3}, {"x": 0, "y": 4}]',
+			horizontalLine5: '[{"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0}, {"x": 3, "y": 0}, {"x": 4, "y": 0}]'
+		}
+	};
+})(window);
+
+createjs.Touch.enable(config.stage);
