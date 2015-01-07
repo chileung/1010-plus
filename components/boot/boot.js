1a4a3832940a6fa23cd806ceeaf57d7e72a939de
chileung <kwanchileung4816@gmail.com>
2015-01-07 15:18:42 +0800

    [UPD]调整代码架构

--- components/boot/boot.js
+++ components/boot/boot.js
@@ -1,5 +1,23 @@
 'use strict';
 
-module.exports = function () {
-    console.log('hello world');
+module.exports = function() {
+	// main
+	var counter = new Counter();
+	var playground = new Kursaal({
+		counter: counter
+	});
+	var generator = new RandomBrickGenerator({
+		enableMoving: true
+	});
+
+	// 设置生成器要服务的娱乐场
+	generator.setKursaal(playground);
+
+	// 摆放位置
+	generator.moveTo(30, playground.mapHeight + 100 + config.size * 1.5);
+	playground.moveTo((config.stage.canvas.width - playground.mapWidth) / 2, 100);
+	counter.moveTo((config.stage.canvas.width - playground.mapWidth) / 2, 50);
+
+	// 启动游戏
+	generator.start();
 };
