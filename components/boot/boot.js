ce207f5c77cbf1a5b857a268ed4f02d9657d7f64
chileung <kwanchileung4816@gmail.com>
2015-01-08 11:05:13 +0800

    [ADD]添加page.js，支持SPA

--- components/boot/boot.js
+++ components/boot/boot.js
@@ -1,32 +1,7 @@
 'use strict';
 
-var Counter = require('c-counter');
-var config = require('config');
-var Kursaal = require('c-kursaal');
-var RandomBrickGenerator = require('c-randombrickgenerator');
-
 module.exports = function() {
-	createjs.Touch.enable(config.stage);
-	createjs.Ticker.setFPS(75);
-	createjs.Ticker.addEventListener('tick', config.stage);
-
-	// main
-	var counter = new Counter();
-	var playground = new Kursaal({
-		counter: counter
+	require.async('page', function(page) {
+		page.start('p-main');
 	});
-	var generator = new RandomBrickGenerator({
-		enableMoving: true
-	});
-
-	// 设置生成器要服务的娱乐场
-	generator.setKursaal(playground);
-
-	// 摆放位置
-	generator.moveTo(30, playground.mapHeight + 100 + config.size * 1.5);
-	playground.moveTo((config.stage.canvas.width - playground.mapWidth) / 2, 100);
-	counter.moveTo((config.stage.canvas.width - playground.mapWidth) / 2, 50);
-
-	// 启动游戏
-	generator.start();
 };
