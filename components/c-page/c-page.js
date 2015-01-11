35a0fbb70d60fea2d0886eacb22ad24d7fe448a8
chileung <kwanchileung4816@gmail.com>
2015-01-11 22:34:06 +0800

    [UPD]完善page.js

--- components/c-page/c-page.js
+++ components/c-page/c-page.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/*
+  c-page.js
+    A simple module that makes your Web app supports SPA(Single Page Application).
+  Author: chileung
+  Date: 2015.1.11
+*/
+
 function showRoot(self) {
   if (self.root) {
     self.root.style.display = 'block';
@@ -12,16 +19,28 @@ function hideRoot(self) {
   }
 }
 
+// Page Class
+// Note: Each page in your Web app is an instance of Page. 
 var Page = function(options) {
   var self = this;
 
+  // Root property refer to the root DOM node of the current page.
   self.root = null;
+  // The property ensures your page will only insert into body once.
   self.loaded = false;
 
-  // handler queue of show, hide & load 
+  // Handler queue of show, hide & load 
   var showFnList = [];
   var hideFnList = [];
   var loadFnList = [];
+
+  /* Note: 
+      Each page has three methods which will be invoked at the specific time.
+      When the new page is going to be shown, the "show" method of the new page will be invoked,
+    as well as the "hide" method of the current page which will be hidden, will be invoked. Note 
+    that if the new page has not been initialized and inserted into body, the load method will be 
+    invoked before the show method and only invoked once.
+  */
 
   Object.defineProperties(self, {
     show: {
@@ -70,6 +89,7 @@ var Page = function(options) {
               fn.call(self, state);
             });
 
+            // ensures that the load method of the page will only invoked once.
             self.loaded = true;
           }
         };
