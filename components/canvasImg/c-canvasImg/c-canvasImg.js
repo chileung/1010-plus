90d89380c51b7efc00bd0216ed6f926e69cd1b2b
chileung <kwanchileung4816@gmail.com>
2015-01-13 10:01:51 +0800

    [UPD]完善注释

--- components/canvasImg/c-canvasImg/c-canvasImg.js
+++ components/canvasImg/c-canvasImg/c-canvasImg.js
@@ -3,17 +3,23 @@
 /* 部分图片使用canvas绘制，因此将逻辑进行抽象
    约定：
     1. 使用canvas标签，且必须带有属性值为"false"的data-init属性
-    2. 继承该类的子类必须声明一个className属性，属性值为子类图片所特有的一个HTML类名
+    2. 类实例化时必须传入一个className，作为子类图片所特有的一个HTML类名
+    3. 子类继承时需要声明width和height属性
     3. 子类需要重写父类的draw方法，以描绘自己的形状
     4. 子类实例化后，调用initialize()方法，即可在dom中绘制图片
 */
 
 module.exports = Object.subClass({
-  initialize: function() {
+  init: function(className) {
+    this.className = className || '';
+  },
+  initialize: function() {  
     var nodes = document.querySelectorAll('canvas[data-init="false"].' + (this.className || ''));
 
     for (var i = 0, l = nodes.length; i < l; i++) {
       this.node = nodes[i];
+      this.node.setAttribute('width', this.width || 0);
+      this.node.setAttribute('height', this.height || 0);
       this.draw();
       this.node.dataset.init = 'true';
     }
