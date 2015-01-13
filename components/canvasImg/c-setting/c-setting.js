adad31a2c7b83b81ec0148a9dc031ede48459eb8
chileung <kwanchileung4816@gmail.com>
2015-01-13 10:03:14 +0800

    [UPD]完成设置按钮的描绘，作为其他图案的按考

--- components/canvasImg/c-setting/c-setting.js
+++ components/canvasImg/c-setting/c-setting.js
@@ -3,11 +3,33 @@
 var CanvasImg = require('canvasImg/c-canvasImg');
 
 var Setting = CanvasImg.subClass({
-  className: 'c-setting',
+  width: 50,
+  height: 50,
   // overwrite
   draw: function() {
-    // todo
+    var ctx = this.node.getContext('2d');
+    var color = 'gray';
+
+    var g = new createjs.Graphics();
+    g.s(color)
+      .f(color)
+      .dc(25, 25, 15)
+      .s('#fff')
+      .f('#fff')
+      .dc(25, 25, 10);
+
+    g.draw(ctx);
+
+    var cos = Math.cos;
+    var sin = Math.sin;
+    var PI = Math.PI;
+
+    for (var r = 0; r < 2 * PI; r += 2 * PI / 8) {
+      g = new createjs.Graphics();
+      g.s(color).f(color).dc(15 * cos(r) + 25, 15 * sin(r) + 25, 3);
+      g.draw(ctx);
+    }
   }
 });
 
-module.exports = new Setting();
+module.exports = new Setting('c-setting');
